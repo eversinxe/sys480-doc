725df1040ad08393e5c060c0471566a77f5e6338
eversinxe <45649109+eversinxe@users.noreply.github.com>
2021-10-28 11:54:37 -0400

    Update 480-utils.ps1

--- 480-utils.ps1
+++ 480-utils.ps1
@@ -1,12 +1,16 @@
 Function Read-HostDefault([string]$prompt, [string]$default)
 {
-
+    if(!prompt){
+        Write-Host "Within the default value"
+    }else {
+        Write-Host "You chose:" $prompt
+    }
 }
 
 Function connect([string] $server)
 {
     $conn = $global:DefaultVIServer
-    # are we connected?
+    # to see if connected
     if ($conn){
         $msg = "connected to: {0}" -f $conn
 
@@ -17,3 +21,71 @@ Function connect([string] $server)
     }
     return $conn
 }
+
+## function check_index([int]$selection.[int]$max)
+{
+    
+}
+function pick_host()
+{
+    Get-VMHost | Select-Object Name
+    $vsphere_host = Read-Host "Pick a server to host vm ["$global:config.vm_host"]"
+    Read-HostDefault($vsphere_host)
+    if (!vsphere_host) {
+        $global:vm_host = Get-VMHost -Name $global:config.vm_host
+    } else {
+        $global:vm_host = Get-VMHost -Name $vsphere_host
+    }
+}
+
+function pick_vm([string] $folder)
+{
+    Get-Folder | -Type VM | Select-Object Name
+    $folder = Read-Host "Which folder you want to clone a VM ["$global:config.base_folder"]"
+    Read-HostDefault($folder)
+    if(!$folder){
+        $basefolder = Get-Folder -Name $global:config.base_folder
+    } else {
+        $basefolder = Get-Folder -Name $folder 
+    }
+    $vm_list = $basefolder | Get-VM
+    foreach ($vm in $vm_list)
+    {
+        Write-Host $vm.Name
+    }
+}
+
+function pick_datastore()
+{
+    Get-Datastore | Select-Object Name
+    $datastore = Read-Host "Choose the datastore ["$global:config.datastore"]"
+    Read-HostDefault($datastore)
+    if (!$datastore){
+        $global:datastore = Get-VMHost -Name $global:config.datastore
+    } else {
+        $global:datastore = Get-VMHost -Name $datastore
+    }
+    $datastore_list = $datastore | Get-Datastore
+    foreach ($datastore in $datastore_list)
+    {
+        Write-Host $datastore.Name
+    }
+}
+
+function pick_network([string] $vmhost_name)
+{
+    
+}
+
+function get_config([string] $config_path)
+{
+
+}
+
+function cloner($config_path)
+{
+
+}
+
+#temporary main
+cloner -config_path *./480-utils.json*
